d17e7fde0ddfec1d97c7d969498388a7ce547813
sdc mac <jielin.zhang447@gmail.com>
2018-06-11 23:08:05 +0800

    完善首页图表，嵌入数据

--- start.py
+++ start.py
@@ -18,7 +18,7 @@ login_manager = LoginManager()
 app = Flask(__name__)
 # db = SQLAlchemy(app)
 # URI -> mysql://username:password@server/db
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:admin@localhost:3306/gas'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:CHARLIE4494@localhost:3306/gas'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db = SQLAlchemy(app)
 login_manager.init_app(app)
